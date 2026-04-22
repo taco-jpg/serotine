ca693ff10c0f166f48bef05863918a56db40e84a
Louis Liu <dorbort123@gmail.com>
2026-04-22 05:04:50 +0000

    Update PrismaClient import to use edge version and add output path in schema

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 import { PrismaD1 } from '@prisma/adapter-d1'
 import { getRequestContext } from '@cloudflare/next-on-pages'
 
